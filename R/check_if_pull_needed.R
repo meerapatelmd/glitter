ce217fc51da143c40ad12c35c60c5802e436788f
Meera Patel <patelm9@mskcc.org>
2020-04-04 23:22:58 -0400

    add check_if_pull_needed function

---
+++ R/check_if_pull_needed.R
@@ -0,0 +1,13 @@
+#' CHeck if pull for a given repo is needed
+#' @param path_to_local_repo path to local repo
+#' @export
+
+check_if_pull_needed <-
+        function(path_to_local_repo) {
+                git_msg <- pull(path_to_local_repo = path_to_local_repo)
+                if (git_msg[1] == "Already up to date.") {
+                        return(TRUE)
+                } else {
+                        return(FALSE)
+                }
+        }

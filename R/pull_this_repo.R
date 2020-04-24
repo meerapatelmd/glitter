4fd0183f6bafb447ee408830088d8c9175750b16
Meera Patel <patelm9@mskcc.org>
2020-04-24 19:38:29 -0400

    update all changes to functions

--- R/pull_this_repo.R
+++
@@ -1,8 +0,0 @@
-#' Pull a GitHub repo based on the path to local repo
-#' @export
-
-pull_this_repo <-
-        function() {
-                        x <- system("git pull", intern = TRUE)
-                        return(x)
-        }

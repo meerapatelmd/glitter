209f0383157164b54856aa921996b468f195e675
Meera Patel <patelm9@mskcc.org>
2020-04-29 16:31:32 -0400

    add add_commit_all_deltas_in_path function

---
+++ R/add_commit_all_deltas_in_path.R
@@ -0,0 +1,20 @@
+#' Adds and commits all changed files in a given path
+#' @export
+
+
+add_commit_all_deltas_in_path <-
+        function(path, commit_message = NULL, description = NULL) {
+                all_deltas <- c(modified_files(path_to_local_repo = path),
+                                untracked_files(path_to_local_repo = path),
+                                deleted_files(path_to_local_repo = path))
+
+                all_deltas_in_path <- grep("^[.]{2]", all_deltas, invert = TRUE, value = TRUE)
+
+                git_msg <-
+                        add_commit_some(path_to_local_repo = getwd(),
+                                        filenames = all_deltas_in_path,
+                                        commit_message = commit_message)
+
+                pretty_if_exists(git_msg = git_msg)
+                invisible(git_msg)
+        }

8f040aacd636418a9654e44197585f5bbdcdde00
Meera Patel <patelm9@mskcc.org>
2020-05-15 23:27:13 -0400

    add 100MB size filter for add_commit_some generic function

--- R/add_commit_some.R
+++ R/add_commit_some.R
@@ -5,49 +5,64 @@
 #' @importFrom secretary typewrite_error
 #' @importFrom purrr keep
 #' @importFrom rubix map_names_set
+#' @importFrom crayon italic
 #' @export
 
-
 add_commit_some <-
-        function(path_to_local_repo, filenames, commit_message = NULL, description = NULL, verbose = TRUE) {
+        function(path_to_local_repo, filenames, commit_message = NULL, description = NULL, log = FALSE) {
                 if (dir.exists(path_to_local_repo)) {
+
+                        input_filenames <- filenames
 
                         path_to_filenames <- paste0(path_to_local_repo, "/", filenames)
 
 
                         ##Filtering for only files less than 100 MB
-                        filenames <-
+                        path_to_filenames <-
                         path_to_filenames %>%
                                 rubix::map_names_set(cave::size_in_mb) %>%
                                 purrr::keep(function(x) x < 100) %>%
                                 names()
 
-                        for (i in 1:length(filenames)) {
-                                fn <- sanitize_fns_for_cli(filenames[i])
-                                x <- system(paste0("cd\n",
-                                                                 "cd ", path_to_local_repo,"\n",
-                                                                 "git add ", fn),
-                                                           intern = TRUE)
-                        }
+                        filenames <- basename(path_to_filenames)
 
-                        if (!is.null(commit_message)) {
-                                x <-
-                                        commit(path_to_local_repo = path_to_local_repo,
-                                               commit_message = commit_message,
-                                               description = description)
+                        if (length(filenames) > 0) {
 
+                                        while (length(filenames) > 0) {
+                                                fn <- filenames[1]
+                                                fn <- sanitize_fns_for_cli(fn)
+                                                x <- system(paste0("cd\n",
+                                                                   "cd ", path_to_local_repo,"\n",
+                                                                   "git add ", fn),
+                                                            intern = TRUE)
+
+                                                filenames <- filenames[-1]
+                                        }
+
+                                        if (!is.null(commit_message)) {
+                                                x <-
+                                                        commit(path_to_local_repo = path_to_local_repo,
+                                                               commit_message = commit_message,
+                                                               description = description)
+
+                                        } else {
+                                                x <-
+                                                        commit(path_to_local_repo = path_to_local_repo,
+                                                               commit_message = paste0("change to ", paste(filenames, collapse = ", "), " written in ", cave::present_script_path()),
+                                                               description = description)
+                                        }
+
+                                        secretary::typewrite_bold("\nCommit Status:")
+                                        pretty_if_exists(x)
+                                        invisible(x)
                         } else {
-                                x <-
-                                        commit(path_to_local_repo = path_to_local_repo,
-                                               commit_message = paste0("change to ", paste(filenames, collapse = ", "), " written in ", cave::present_script_path()),
-                                               description = description)
+                                secretary::typewrite_error("Filenames", crayon::italic(paste(input_filenames, collapse = ", ")), "are not commitable. They either do not exist or are larger than 100MB.", log = log)
                         }
-
-                        secretary::typewrite_bold("\nCommit Status:")
-                        pretty_if_exists(x)
-                        invisible(x)
 
                 } else {
-                        secretary::typewrite_error("Local repository", path_to_local_repo, "does not exist.")
+                        secretary::typewrite_error("Local repository",
+                                                   crayon::italic(path_to_local_repo),
+                                                   "does not exist.",
+                                                   log = log)
                 }
         }

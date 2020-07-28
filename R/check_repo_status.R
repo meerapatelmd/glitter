33dc62b92d78bad1feb2e73adfd01b7e7af9822d
Meera Patel <patelm9@mskcc.org>
2020-07-28 03:44:14 -0400

    remove references to mirCat package

--- R/check_repo_status.R
+++ R/check_repo_status.R
@@ -1,15 +1,15 @@
 #' Check whether a commit is needed or not for a given repo
 #' @param path_to_local_repo path to the local repo
-#' @importFrom mirCat typewrite
+#' @importFrom secretary typewrite
 #' @export
 
 check_repo_status <-
         function(path_to_local_repo) {
                 x <- status(path_to_local_repo = path_to_local_repo)
                 if ("nothing to commit, working tree clean" %in% x) {
-                       mirCat::typewrite("\tnothing to commit, working tree clean\n")
+                       secretary::typewrite("\tnothing to commit, working tree clean\n")
                 } else {
-                        mirCat::typewrite("\tcommits needed:\n")
+                        secretary::typewrite("\tcommits needed:\n")
                         pretty(x)
                         invisible(x)
                 }

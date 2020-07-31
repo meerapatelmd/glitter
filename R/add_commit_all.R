e9ecc262f7262177c71211b1fba21bb3a754e9fa
Meera Patel <patelm9@mskcc.org>
2020-07-31 19:54:12 -0400

    bug fix add_commit_all function

--- R/add_commit_all.R
+++ R/add_commit_all.R
@@ -18,10 +18,23 @@ add_commit_all <-
 
                 add_all(path_to_local_repo = path_to_local_repo)
 
-                commit(path_to_local_repo = path_to_local_repo,
-                       commit_message = commit_message,
-                       description = description,
-                       verbose = verbose
-                       )
+                statusResponse <- status(path_to_local_repo = path_to_local_repo,
+                                         verbose = FALSE)
+
+                statusResponse <- statusResponse %>%
+                                        centipede::no_blank()
+
+                if (!all(statusResponse %in% c("On branch master","Your branch is up to date with 'origin/master'.","nothing to commit, working tree clean"))) {
+
+
+                        commit(path_to_local_repo = path_to_local_repo,
+                               commit_message = commit_message,
+                               description = description,
+                               verbose = verbose
+                        )
+
+
+                }
+
         }
 

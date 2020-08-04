086df3fdcc26c5988fd271616d75bc6a096cf058
Meera Patel <patelm9@mskcc.org>
2020-08-04 10:14:04 -0400

    add Issues functions

---
+++ R/ghiFunctions.R
@@ -0,0 +1,137 @@
+#' GitHub Issues
+#' @description https://github.com/stephencelis/ghi
+#' @export
+
+listOpenIssues <-
+        function(repo = NULL) {
+
+                if (is.null(repo)) {
+
+                        repo <- getwd()
+
+                }
+
+                system(paste0("cd\n",
+                              "cd ", repo,"\n",
+                              "ghi list"),
+                       intern = FALSE)
+
+
+                output <-
+                        tibble::tibble(Issues = system(paste0("cd\n",
+                                                     "cd ", repo,"\n",
+                                                     "ghi list"),
+                                                        intern = TRUE)[-1]) %>%
+                        tidyr::extract(col = Issues,
+                                       into = c("IssueNo",
+                                                "OpenIssue"),
+                                       regex = "(^[ ]{0,}[0-9]{1,}[ ]*?)([a-zA-Z]{1,}.*$)") %>%
+                        dplyr::mutate_all(trimws, "both")
+
+                invisible(output)
+
+        }
+
+#' GitHub Issues
+#' @description https://github.com/stephencelis/ghi
+#' @export
+
+listClosedIssues <-
+        function(repo = NULL) {
+
+                if (is.null(repo)) {
+
+                        repo <- getwd()
+
+                }
+
+                system(paste0("cd\n",
+                              "cd ", repo,"\n",
+                              "ghi close --list"),
+                       intern = FALSE)
+
+
+                output <-
+                        tibble::tibble(Issues = system(paste0("cd\n",
+                                                              "cd ", repo,"\n",
+                                                              "ghi close --list"),
+                                                       intern = TRUE)[-1]) %>%
+                        tidyr::extract(col = Issues,
+                                       into = c("IssueNo",
+                                                "ClosedIssue"),
+                                       regex = "(^[ ]{0,}[0-9]{1,}[ ]*?)([a-zA-Z]{1,}.*$)") %>%
+                        dplyr::mutate_all(trimws, "both")
+
+                invisible(output)
+        }
+
+
+#' Open a GitHub Issue
+#' @description This function opens a GitHub Issue from the R console.
+#' @export
+
+openIssue <-
+        function(repo = NULL,
+                 title,
+                 description) {
+
+                if (is.null(repo)) {
+
+                        repo <- getwd()
+
+                }
+
+                prompt <-
+                paste0("cd\n",
+                       "cd ", repo,"\n",
+                       "ghi open -m '", description, "' \ '", title, "'")
+
+                system(prompt)
+        }
+
+
+#' Close a GitHub Issue
+#' @description This function closes a GitHub Issue from the R console.
+#' @export
+
+closeIssue <-
+        function(repo = NULL,
+                 issueNo,
+                 closureMsg) {
+
+                if (is.null(repo)) {
+
+                        repo <- getwd()
+
+                }
+
+                prompt <-
+                        paste0("cd\n",
+                               "cd ", repo,"\n",
+                               "ghi close -m '", closureMsg, "' \ '", closureMsg, "' ", issueNo)
+
+                system(prompt)
+        }
+
+
+#' Show a GitHub Issue
+#' @description This function shows a GitHub Issue from the R console.
+#' @export
+
+showIssue <-
+        function(repo = NULL,
+                 issueNo) {
+
+                if (is.null(repo)) {
+
+                        repo <- getwd()
+
+                }
+
+                prompt <-
+                        paste0("cd\n",
+                               "cd ", repo,"\n",
+                               "ghi show ", issueNo)
+
+                system(prompt)
+        }

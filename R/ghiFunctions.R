3c38079434c9b7cf3daa43ca5f6825af20e9395e
Meera Patel <patelm9@mskcc.org>
2020-08-05 21:15:48 -0400

    update description for ghiFunctions

--- R/ghiFunctions.R
+++ R/ghiFunctions.R
@@ -1,5 +1,43 @@
-#' GitHub Issues
-#' @description https://github.com/stephencelis/ghi
+#' List Open GitHub Issues
+#' @description
+#' All GitHub Issues functions in this package requires downloading and setting up authorization for \href{https://github.com/stephencelis/ghi}{GHI}.
+#' @export
+
+listOpenIssues <-
+        function(repo = NULL) {
+
+                if (is.null(repo)) {
+
+                        repo <- getwd()
+
+                }
+
+                system(paste0("cd\n",
+                              "cd ", repo,"\n",
+                              "/usr/local/bin/ghi list"),
+                       intern = FALSE)
+
+
+                output <-
+                        # Converting the vector of issues into a dataframe
+                        tibble::tibble(Issues = system(paste0("cd\n",
+                                                              "cd ", repo,"\n",
+                                                              "/usr/local/bin/ghi list"),
+                                                       intern = TRUE))  %>%
+                        # Removing theheader to isolate the issue number and issue name
+                        rubix::filter_at_grepl(Issues,
+                                               grepl_phrase = "[#]{1}.*open issues$",
+                                               evaluates_to = FALSE)  %>%
+                        tidyr::extract(col = Issues,
+                                       into = c("IssueNo",
+                                                "OpenIssue"),
+                                       regex = "^[ ]+?([0-9]+?)[ ]+?([^ ]{1}.*$)")
+
+                invisible(output)
+
+        }
+
+#' List Closed Issues
 #' @export
 
 listClosedIssues <-

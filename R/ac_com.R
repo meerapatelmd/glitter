a9871b13c097d875048449a297d0f560468513dc
Meera Patel <patelmeeray@gmail.com>
2020-09-24 01:15:21 -0400

    rewrite functions

---
+++ R/ac_com.R
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+com <-
+        function(commit_msg,
+                 path_to_local_repo = NULL,
+                 verbose = TRUE) {
+
+                mk_local_path_if_null(path_to_local_repo = path_to_local_repo)
+
+                command <-
+                        c(starting_command(path_to_local_repo = path_to_local_repo),
+                          paste0("git commit -m '", commit_msg, "'")) %>%
+                        paste(collapse = "\n")
+
+                commit_response <-
+                        system(command = command,
+                               intern = TRUE)
+
+                if (verbose) {
+                        cat(commit_response, sep = "\n")
+                }
+
+                invisible(commit_response)
+        }
+
+
+
+
+ac <-
+        function(...,
+                 all = FALSE,
+                 path_to_local_repo = NULL,
+                 max_mb = 50)
+
+                {
+
+
+                add(...,
+                    all = all,
+                    path_to_local_repo = path_to_local_repo,
+                    max_mb = max_mb)
+
+
+
+
+
+
+
+        }
--- R/add.R
+++ R/add.R
@@ -1,17 +1,83 @@
-
-
-
-
-
-
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param ... PARAM_DESCRIPTION
+#' @param all PARAM_DESCRIPTION, Default: FALSE
+#' @param path_to_local_repo PARAM_DESCRIPTION, Default: NULL
+#' @param max_mb PARAM_DESCRIPTION, Default: 50
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso
+#'  \code{\link[tibble]{tibble}}
+#'  \code{\link[dplyr]{filter}},\code{\link[dplyr]{select}}
+#'  \code{\link[purrr]{map}}
+#' @rdname add
+#' @export
+#' @importFrom tibble tibble
+#' @importFrom dplyr filter select
+#' @importFrom purrr map
 
 
 add <-
         function(...,
+                 all = FALSE,
                  path_to_local_repo = NULL,
                  max_mb = 50) {
 
                 mk_local_path_if_null(path_to_local_repo = path_to_local_repo)
+
+
+                if (all) {
+
+
+                        files <-
+                                list.files(path_to_local_repo,
+                                           recursive = TRUE)
+
+                        file_mb <- file.info(files)$size/(10^6)
+
+                        big_files <-
+                                tibble::tibble(files = files,
+                                               file_mb = file_mb) %>%
+                                dplyr::filter(file_mb >= max_mb) %>%
+                                dplyr::select(files) %>%
+                                unlist() %>%
+                                unname()
+
+                        if (length(big_files)) {
+
+                                files <- files[!(files %in% big_files)]
+
+                                command <-
+                                        c(starting_command(path_to_local_repo = path_to_local_repo),
+                                          files %>%
+                                                  purrr::map(~paste0("git add ", .)) %>%
+                                                  unlist()) %>%
+                                        paste(collapse = "\n")
+
+                                system(command = command)
+
+
+                        } else {
+
+
+                                command <-
+                                        c(starting_command(path_to_local_repo = path_to_local_repo),
+                                          "git add .") %>%
+                                        paste(collapse = "\n")
+
+
+                                system(command = command)
+
+                        }
+
+
+                } else {
 
                 Args <- unlist(list(...))
                 Args <- paste(Args, collapse = "|")
@@ -42,5 +108,7 @@ add <-
                         paste(collapse = "\n")
 
                 system(command = command)
+
+                }
 
         }

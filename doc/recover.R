2c4d9958a220680194c1d0be9013319b86628178
Meera Patel <patelmeeray@gmail.com>
2020-11-29 15:16:36 -0500

    update documentation

--- doc/recover.R
+++ doc/recover.R
@@ -4,38 +4,37 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
-## ----setup--------------------------------------------------------------------
+## ----setup,message=FALSE------------------------------------------------------
 library(glitter)
 
 ## ---- eval=TRUE---------------------------------------------------------------
 
-deleted_files <- list_deleted_files(path = "/Users/meerapatel/GitHub/glitter")
-head(deleted_files)
-
-
-## ---- eval=TRUE---------------------------------------------------------------
-
 list_file_commits(file = "R/destroy.R",
-                  path = "/Users/meerapatel/GitHub/glitter")
+                  path = getwd())
 
+
+## ----echo=FALSE,eval=TRUE-----------------------------------------------------
+# QA for vignette to run correctly 
+if (file.exists("R/destroy.R")) {
+    file.remove("R/destroy.R")
+} 
 
 ## -----------------------------------------------------------------------------
 file.exists("R/destroy.R")
 
 ## -----------------------------------------------------------------------------
 recover_lost_file(file = "R/destroy.R",
-                  path = "/Users/meerapatel/GitHub/glitter")
+                  path = getwd())
 
 ## -----------------------------------------------------------------------------
 file.exists("R/destroy.R")
 
 ## -----------------------------------------------------------------------------
 file.remove("R/destroy.R")
-file.exists("R/destroy.R")
 
 ## -----------------------------------------------------------------------------
 recover_lost_file(file = "R/destroy.R",
-                  path = "/Users/meerapatel/GitHub/glitter", 
+                  path = getwd(), 
                   sha = "aa448c7de4ead65905dfb3133ec42ac8a6f332fc")
 
 ## -----------------------------------------------------------------------------

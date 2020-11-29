ee31b650a259e5ad972f5a2b2cd33b82cf4c0f29
Meera Patel <patelmeeray@gmail.com>
2020-11-29 15:32:26 -0500

    update documentation

--- doc/recover.R
+++ doc/recover.R
@@ -1,43 +1,42 @@
 ## ---- include = FALSE---------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
-  comment = "#>",
-  cache = TRUE
+  comment = "#>"
 )
 
 ## ----setup,message=FALSE------------------------------------------------------
 library(glitter)
 
 ## ---- eval=TRUE---------------------------------------------------------------
-
-list_file_commits(file = "R/destroy.R")
-
+list_file_commits(file = "R/destroy.R",
+                  path = "/Users/meerapatel/GitHub/packages/glitter")
 
 ## ----echo=FALSE,eval=TRUE-----------------------------------------------------
 # QA for vignette to run correctly 
-if (file.exists("R/destroy.R")) {
-    file.remove("R/destroy.R")
+path_to_file <- file.path("/Users/meerapatel/GitHub/packages/glitter", "R/destroy.R")
+if (file.exists(path_to_file)) {
+    file.remove(path_to_file)
 } 
 
 ## -----------------------------------------------------------------------------
-file.exists("R/destroy.R")
+file.exists("/Users/meerapatel/GitHub/packages/glitter/R/destroy.R")
 
 ## -----------------------------------------------------------------------------
-recover_lost_file(file = "R/destroy.R")
+recover_lost_file(file = "/Users/meerapatel/GitHub/packages/glitter/R/destroy.R")
 
 ## -----------------------------------------------------------------------------
-file.exists("R/destroy.R")
+file.exists("/Users/meerapatel/GitHub/packages/glitter/R/destroy.R")
 
 ## -----------------------------------------------------------------------------
-file.remove("R/destroy.R")
+file.remove("/Users/meerapatel/GitHub/packages/glitter/R/destroy.R")
 
 ## -----------------------------------------------------------------------------
-recover_lost_file(file = "R/destroy.R", 
+recover_lost_file(file = "/Users/meerapatel/GitHub/packages/glitter/R/destroy.R", 
                   sha = "aa448c7de4ead65905dfb3133ec42ac8a6f332fc")
 
 ## -----------------------------------------------------------------------------
-file.exists("R/destroy.R")
+file.exists("/Users/meerapatel/GitHub/packages/glitter/R/destroy.R")
 
-## ----echo=FALSE,eval=TRUE-----------------------------------------------------
-file.remove("R/destroy.R")
+## ----echo=FALSE,eval=TRUE,message=FALSE---------------------------------------
+file.remove("/Users/meerapatel/GitHub/packages/glitter/R/destroy.R")
 

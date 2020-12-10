28f1b960921680fd0a9fe64f2288e6e871f8fcb5
Meera <patelm9@mskcc.org>
2020-12-10 11:31:03 -0500

    remove mk_local_path_if_null

--- R/helpers.R
+++ R/helpers.R
@@ -1,17 +1,6 @@
 #' @keywords internal
 #' @export
 
-mk_local_path_if_null <-
-        function(path_to_local_repo) {
-
-                if (is.null(path_to_local_repo)) {
-                        assign("path_to_local_repo", getwd(), envir = parent.frame())
-                }
-        }
-
-#' @keywords internal
-#' @export
-
 extend_path <-
         function(...) {
 

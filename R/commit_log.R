942fa1e0a8f7b792d71ab37ce3da265e7ceded32
Meera Patel <patelmeeray@gmail.com>
2020-12-13 20:38:51 -0500

    rename commit_graph to commit_log

---
+++ R/commit_log.R
@@ -0,0 +1,30 @@
+#' See the history of your commits in a graph
+#' @export
+
+commit_log <-
+        function(path = getwd(),
+                 verbose = TRUE,
+                 remove = TRUE) {
+
+
+                log_response <- system(paste0("cd\n", "cd ", path, "\n git log --oneline --graph --all"), intern = TRUE)
+
+                if (verbose) {
+                        printMsg(log_response)
+                }
+
+
+                log_response_df <-
+                tibble::tibble(log_response = log_response) %>%
+                        tidyr::extract(col = log_response,
+                                       into = c("garbage", "sha", "commit_msg"),
+                                       regex = "(^[*] )([a-zA-Z0-9]{7})(.*$)",
+                                       remove = remove) %>%
+                        dplyr::select(-garbage) %>%
+                        dplyr::mutate_all(as.character) %>%
+                        dplyr::mutate_all(trimws) %>%
+                        dplyr::filter_all(dplyr::all_vars(!is.na(.)))
+
+                invisible(log_response_df)
+
+        }

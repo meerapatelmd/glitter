0eacd4df5c1229a1d4fde14ed36c409ee039189a
Meera Patel <patelmeeray@gmail.com>
2020-12-13 21:09:27 -0500

    add lfs feature

--- R/lfs.R
+++ R/lfs.R
@@ -1,18 +1,29 @@
-#' Push a local repo to remote MSK KMI Enterprise GitHub repository
-#' @param large_file_extensions extensions of files that will be large and will need to be tracked using the lfs
-#' @keywords internal
+#' @title
+#' Install LFS
+#' @param lfs_ext extensions of files that will be large and will need to be tracked using the lfs
 #' @export
 
+install_lfs <-
+        function(path = getwd(), lfs_ext = c("xlsx", "csv", "RData")) {
 
-lfs_install_this_repo <-
-        function(large_file_extensions = c("xlsx", "csv", "RData")) {
-                system("git lfs install")
-                cat("\n")
-                large_file_extensions %>%
-                        purrr::map(function(x) system(paste0("git lfs track '*.", x,"'")))
+                path <- extend_path(path)
 
-                cat("\n")
-                system("git add .gitattributes")
-                cat("\n")
-                system("git commit -m '+: .gitattribute for lfs tracking'")
+                command1 <-
+                sprintf(
+                        "cd\n
+                        cd %s\n
+                        git lfs install\n", path
+                        )
+
+                command2 <-
+                        sprintf("git lfs track '*.%s'\n", lfs_ext)
+
+                command3 <- "git add .gitattributes\ngit commit -m '+: .gitattribute for lfs tracking'\n"
+
+                command <- sprintf("%s%s%s", command1, command2, command3)
+
+                response <- system(command = command,
+                                   intern = TRUE)
+
+                print_response(response)
         }

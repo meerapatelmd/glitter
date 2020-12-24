27c109123e08fb036855b22a24161853a1a195f9
Meera Patel <patelmeeray@gmail.com>
2020-12-23 19:16:55 -0500

    add pipe back tp package

--- R/utils.R
+++ R/utils.R
@@ -1,24 +1,3 @@
-#' Pipe operator
-#'
-#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
-#'
-#' @name %>%
-#' @rdname pipe
-#' @export
-#' @importFrom magrittr %>%
-#' @usage lhs \%>\% rhs
-
-`%>%` <-
-        function (lhs, rhs)
-        {
-                lhs <- substitute(lhs)
-                rhs <- substitute(rhs)
-                kind <- 1L
-                env <- parent.frame()
-                lazy <- TRUE
-                .External2(magrittr_pipe)
-        }
-
 #' Subsitute spaces and special characters with the backslash for a CLI command
 #' @export
 

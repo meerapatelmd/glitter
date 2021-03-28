9b61db3ab97e0df711b390a503e53e84bd708e4d
Meera Patel <patelmeeray@gmail.com>
2021-03-28 16:30:58 -0400

    add `copy_branch()`

--- R/main_master.R
+++ R/main_master.R
@@ -29,3 +29,54 @@ is_master <-
 
                 "master" %in% repo_branches
         }
+
+
+
+#' @title
+#' Copy Branch
+#'
+#' @description
+#' Copy an existing branch to a new branch with the provided
+#' name. To rename default branches, such as  `main` to
+#' `master`, this function is run followed by pointing the
+#' repo to the new branch as the default before deleting the
+#' original `main` branch with:
+#' ```
+#' git push origin --delete main
+#' ```
+#' See
+#' \href{https://www.git-tower.com/learn/git/faq/git-rename-master-to-main/}{Git Tower FAQ: Rename Master to Main}
+#' for full details.  .
+#'
+#'
+#'
+
+copy_branch <-
+        function(path = getwd(),
+                 branch,
+                 new_branch_name,
+                 remote_branch = "origin") {
+
+
+        path <- normalizePath(
+                  path.expand(path = path),
+                  mustWork = TRUE)
+
+
+        command <-
+                c("cd",
+                sprintf("cd %s", path),
+                sprintf("git branch -m %s %s",
+                        branch,
+                        new_branch_name),
+                sprintf("git push -u %s %s",
+                        remote_branch,
+                        new_branch_name)) %>%
+                paste(collapse = "\n")
+
+        system(command = command)
+
+
+
+
+        }

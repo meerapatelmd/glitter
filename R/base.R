9be67a0d65d4a048f4025519727d28be5c8f3011
Meera Patel <patelmeeray@gmail.com>
2021-08-23 20:51:45 -0400

    deploy pkg and GitHub Pages using glitter

--- R/base.R
+++ R/base.R
@@ -238,7 +238,51 @@ pull <-
 
 
 
+#' @title
+#' Pull a Remote Repo
+#' @rdname pull_ff
+#' @export
+#' @importFrom magrittr %>%
 
+pull_ff <-
+  function(path = getwd(),
+           verbose = TRUE,
+           ...) {
+
+
+    command <-
+      c(
+        starting_command(path = path),
+        "git pull -ff"
+      ) %>%
+      paste(collapse = "\n")
+
+
+    if (!missing(...)) {
+
+      command <-
+        c(command,
+          paste(unlist(rlang2::list2(...)),
+                collapse = " ")) %>%
+        paste(collapse = "\n")
+
+    }
+
+    pull_response <-
+      system(
+        command = command,
+        intern = TRUE
+      )
+
+    if (verbose) {
+      cat("\n")
+      secretary::typewrite_bold(secretary::magentaTxt("\tPull Response:"))
+      cat(paste0("\t\t", pull_response), sep = "\n")
+      cat("\n")
+    }
+
+    invisible(pull_response)
+  }
 
 
 
@@ -257,7 +301,7 @@ pull <-
 #' @importFrom magrittr %>%
 #' @importFrom rlang list2
 
-push <-
+push_ff <-
   function(path = getwd(),
            remote_name = "origin",
            remote_branch = "master",
